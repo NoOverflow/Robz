2dead061bfd77dc7cec9af2775c3c4fa3f841c3f
NoOverflow <julien.prost@epitech.eu>
2020-07-13 18:00:24 +0200

    Add basic parser for YOLO model

---
+++ Robz/Detection/Dimension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Robz.Detection
+{
+    public class Dimension
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Height { get; set; }
+        public float Width { get; set; }
+    }
+}
